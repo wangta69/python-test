a4914958cfe97aa4aa1bbc1bcdc70ef689a4df2a
YoungHyeong Ryu <wangta69@naver.com>
2017-11-21 15:59:55 +0900

    initial
    initial

--- blog/admin.py
+++ blog/admin.py
@@ -2,9 +2,8 @@ from django.contrib import admin
 from .models import Post
 # Register your models here.
 
-#Username (leave blank to use 'root'): winkzone
+#Username (leave blank to use 'root'): wi*****e
 #Email address: wangta69@naver.com
-#Password:winkzone)(*
-
+#Password:w******(*
 
 admin.site.register(Post)
