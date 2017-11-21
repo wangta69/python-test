f2d6e672be8ca8cef35c353c49b735408ca19a02
YoungHyeong Ryu <wangta69@naver.com>
2017-11-21 15:55:54 +0900

    Initial commit

---
+++ blog/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Post
+# Register your models here.
+
+#Username (leave blank to use 'root'): winkzone
+#Email address: wangta69@naver.com
+#Password:winkzone)(*
+
+
+admin.site.register(Post)
